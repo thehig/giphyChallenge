7ca282521ff70260b691e05f4549fac9e8b58e03
David Higgins <thehig@gmail.com>
2017-10-23 22:36:28 +0100

    Add tests for fetchGiphy

--- giphy-react-app/src/redux/actions/fetchGiphy.test.js
+++ giphy-react-app/src/redux/actions/fetchGiphy.test.js
@@ -3,8 +3,10 @@ import configureMockStore from "redux-mock-store";
 import thunk from "redux-thunk";
 import nock from "nock";
 import { expect } from "chai";
+import { fromJS } from "immutable";
 
 import "../../../test/beforeAll.js";
+import { exampleSearch } from "./fetchGiphy.mock";
 
 import {
   FETCH_GIPHY_BEGIN,
@@ -20,103 +22,86 @@ const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
 describe("fetchGiphy", () => {
-  afterEach(() => {
-    nock.cleanAll();
-  });
+  describe("dispatches", () => {
+    afterEach(() => {
+      nock.cleanAll();
+    });
 
-  // it("handles fetchGiphy success", () => {
+    // it(`${FETCH_GIPHY_DISMISS_ERROR}`, () => {
+    //   expect(dismissFetchGiphyError()).to.deep.equal({
+    //     type: FETCH_GIPHY_DISMISS_ERROR
+    //   });
+    // });
 
-  //   const store = mockStore(initialState);
+    it(`${FETCH_GIPHY_SUCCESS}`, () => {
+      const store = mockStore(initialState);
 
-  //   const list = _.times(2, i => ({
-  //     data: {
-  //       id: `id${i}`,
-  //       title: `test${i}`,
-  //       url: `http://example.com/test${i}`
-  //     }
-  //   }));
-  //   nock("http://www.reddit.com/")
-  //     .get("/r/reactjs.json")
-  //     .reply(200, { data: { children: list } });
+      nock("https://api.giphy.com")
+        .get("/v1/gifs/search")
+        .reply(200, exampleSearch);
 
-  //   return store.dispatch(fetchGiphy()).then(() => {
-  //   //   const actions = store.getActions();
-  //   //   expect(actions[0]).to.have.property(
-  //   //     "type",
-  //   //     HOME_FETCH_REDDIT_REACTJS_LIST_BEGIN
-  //   //   ); //expect
-  //   //   expect(actions[1]).to.have.property(
-  //   //     "type",
-  //   //     HOME_FETCH_REDDIT_REACTJS_LIST_SUCCESS
-  //   //   ); //expect
-  //   }); //then
-  // }); //it
+      return store.dispatch(fetchGiphy()).then(() => {
+        const actions = store.getActions();
+        expect(actions[0]).to.have.property("type", FETCH_GIPHY_BEGIN);
+        expect(actions[1]).to.have.property("type", FETCH_GIPHY_SUCCESS);
+      });
+    }); //it
 
-  //   it("handles fetchGiphy failure", () => {
-  //     nock("http://www.reddit.com/")
-  //       .get("/r/reactjs.json")
-  //       .reply(500, null);
-  //     const store = mockStore({ redditReactjsList: [] });
+    it(`${FETCH_GIPHY_FAILURE}`, () => {
+      const store = mockStore(initialState);
 
-  //     return store.dispatch(fetchGiphy()).catch(() => {
-  //       const actions = store.getActions();
-  //       expect(actions[0]).to.have.property(
-  //         "type",
-  //         HOME_FETCH_REDDIT_REACTJS_LIST_BEGIN
-  //       );
-  //       expect(actions[1]).to.have.property(
-  //         "type",
-  //         HOME_FETCH_REDDIT_REACTJS_LIST_FAILURE
-  //       );
-  //       expect(actions[1]).to.have.nested.property("data.error").that.exist;
-  //     });
-  //   });
+      nock("https://api.giphy.com")
+        .get("/v1/gifs/search")
+        .reply(500, null);
 
-  //   it("dismissfetchGiphyError", () => {
-  //     const expectedAction = {
-  //       type: HOME_FETCH_REDDIT_REACTJS_LIST_DISMISS_ERROR
-  //     };
-  //     expect(dismissfetchGiphyError()).to.deep.equal(expectedAction);
-  //   });
+      return store.dispatch(fetchGiphy()).catch(() => {
+        const actions = store.getActions();
+        expect(actions[0]).to.have.property("type", FETCH_GIPHY_BEGIN);
+        expect(actions[1]).to.have.property("type", FETCH_GIPHY_FAILURE);
+        expect(actions[1]).to.have.nested.property("data.error").that.exist;
+      });
+    }); 
+  }); //describe
 
-  //   it(`reducer should handle ${HOME_FETCH_REDDIT_REACTJS_LIST_BEGIN}`, () => {
-  //     const prevState = { fetchGiphyPending: true };
-  //     const state = reducer(prevState, {
-  //       type: HOME_FETCH_REDDIT_REACTJS_LIST_BEGIN
-  //     });
-  //     expect(state).to.not.equal(prevState);
-  //     expect(state.fetchGiphyPending).to.be.true;
-  //   });
+  describe("reducer", () => {
+    it(`should handle ${FETCH_GIPHY_BEGIN}`, () => {
+      const prevState = fromJS({ fetchGiphyPending: false });
+      const state = reducer(prevState, {
+        type: FETCH_GIPHY_BEGIN
+      });
+      expect(state).to.not.equal(prevState);
+      expect(state.get("fetchGiphyPending")).to.be.true;
+    });
 
-  //   it(`reducer should handle ${HOME_FETCH_REDDIT_REACTJS_LIST_SUCCESS}`, () => {
-  //     const prevState = { fetchGiphyPending: true };
-  //     const state = reducer(prevState, {
-  //       type: HOME_FETCH_REDDIT_REACTJS_LIST_SUCCESS,
-  //       data: { data: { children: [] } }
-  //     });
-  //     expect(state).to.not.equal(prevState);
-  //     expect(state.fetchGiphyPending).to.be.false;
-  //     expect(state.redditReactjsList).to.exist;
-  //   });
+    it(`should handle ${FETCH_GIPHY_SUCCESS}`, () => {
+      const prevState = fromJS({ fetchGiphyPending: true });
+      const state = reducer(prevState, {
+        type: FETCH_GIPHY_SUCCESS,
+        data: { data: { children: [] } }
+      });
+      expect(state).to.not.equal(prevState);
+      expect(state.get("fetchGiphyPending")).to.be.false;
+      expect(state.get("giphyResults")).to.exist;
+    });
 
-  //   it(`reducer should handle ${HOME_FETCH_REDDIT_REACTJS_LIST_FAILURE}`, () => {
-  //     const prevState = { fetchGiphyPending: true };
-  //     const state = reducer(prevState, {
-  //       type: HOME_FETCH_REDDIT_REACTJS_LIST_FAILURE,
-  //       data: { error: new Error("some error") }
-  //     });
-  //     expect(state).to.not.equal(prevState);
-  //     expect(state.fetchGiphyPending).to.be.false;
-  //     expect(state.fetchGiphyError).to.exist;
-  //   });
+    it(`should handle ${FETCH_GIPHY_FAILURE}`, () => {
+      const prevState = fromJS({ fetchGiphyPending: true });
+      const state = reducer(prevState, {
+        type: FETCH_GIPHY_FAILURE,
+        data: { error: new Error("some error") }
+      });
+      expect(state).to.not.equal(prevState);
+      expect(state.get("fetchGiphyPending")).to.be.false;
+      expect(state.get("fetchGiphyError")).to.exist;
+    });
 
-  //   it(`reducer should handle ${HOME_FETCH_REDDIT_REACTJS_LIST_DISMISS_ERROR}`, () => {
-  //     const prevState = { fetchGiphyError: new Error("some error") };
-  //     const state = reducer(prevState, {
-  //       type: HOME_FETCH_REDDIT_REACTJS_LIST_DISMISS_ERROR,
-  //       data: {}
-  //     });
-  //     expect(state).to.not.equal(prevState);
-  //     expect(state.fetchGiphyError).to.be.null;
-  //   });
+    it(`should handle ${FETCH_GIPHY_DISMISS_ERROR}`, () => {
+      const prevState = fromJS({ fetchGiphyError: new Error("some error") });
+      const state = reducer(prevState, {
+        type: FETCH_GIPHY_DISMISS_ERROR
+      });
+      expect(state).to.not.equal(prevState);
+      expect(state.get("fetchGiphyError")).to.be.null;
+    });
+  }); //describe
 });
